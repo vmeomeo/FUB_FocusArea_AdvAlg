c47f7f91fe11517b9921de4ce0aaf052fe87794c
nic.retis@gmail.com <nic.retis@gmail.com>
2025-01-24 16:02:33 +0100

    added search and creation time for the suffix array on suffixarray.py

--- Assignment01/Python/suffixarray_search.py
+++ Assignment01/Python/suffixarray_search.py
@@ -71,10 +71,11 @@ def main(path_reference_genome, path_query_file, num_query):
     suffix_array = iv.create_suffixarray(reference_genome)
     print("Suffix array built.")
 
-
+    suffix_time = time.time() - start_time
     markers = load_markers(path_query_file)
     print('Queries loaded.')
-
+	
+    
     # Check if the number of queries to search for exceeds the number of queries available
     if num_query > len(markers):
         markers = markers * (num_query // len(markers)) + markers[:num_query % len(markers)]
@@ -87,15 +88,16 @@ def main(path_reference_genome, path_query_file, num_query):
         position = search_suffix_array(suffix_array, reference_genome, marker)
         results[marker] = position
 
-    search_time = time.time() - start_time
+    search_time = time.time() - suffix_time
     
 
     # Print the results
     for marker, position in results.items():
         if position:
             print(f"Marker '{marker}' found at position {position}.")
-
-    print(f"Total search time: {search_time:.2f} seconds.")
+            
+    print(f"Suffix array creation time: {suffix_time:.2f} seconds."
+    print(f"Search time time: {search_time:.2f} seconds.")
 
 if __name__ == "__main__":
     # Set up argparse to handle command line arguments
